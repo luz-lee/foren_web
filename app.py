24d8cdfe15724841afa02ef289f729ea8f3cf85f
luz-lee <150001532+luz-lee@users.noreply.github.com>
2024-07-13 19:20:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def result_file(filename):
     return send_from_directory(app.config['RESULT_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8150)
