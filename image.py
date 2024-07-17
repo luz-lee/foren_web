6472ed552942bd599cd3ca8eee6fe0488d0fd6f7
luz-lee <150001532+luz-lee@users.noreply.github.com>
2024-07-17 14:40:41 +0900

    Update image.py

--- image.py
+++ image.py
@@ -4,6 +4,7 @@ import os
 import random
 from utils import resize_image, text_to_image
 
+# 이미지 워터마크 함수
 def apply_watermark(img, text_or_image_path, alpha=5, use_image=False):
     height, width = img.shape[:2]
     if use_image and os.path.exists(text_or_image_path):
@@ -56,3 +57,41 @@ def extract_watermark(img_ori, img_input, alpha=5):
             result2[y, x] = watermark[y_random_indices[y], x_random_indices[x]]
     
     return watermark, result2
+
+def add_and_detect_watermark(image, watermark_text, num_watermarks=5):
+    watermark_positions = []
+
+    h, w, _ = image.shape
+    h_new = (h // 8) * 8
+    w_new = (w // 8) * 8
+    image_resized = cv2.resize(image, (w_new, h_new))
+    
+    ycrcb_image = cv2.cvtColor(image_resized, cv2.COLOR_BGR2YCrCb)
+    y_channel, cr_channel, cb_channel = cv2.split(ycrcb_image)
+    
+    dct_y = cv2.dct(np.float32(y_channel))
+    
+    rows, cols = dct_y.shape
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    for _ in range(num_watermarks):
+        text_size = cv2.getTextSize(watermark_text, font, 0.5, 1)[0]
+        text_x = random.randint(0, cols - text_size[0])
+        text_y = random.randint(text_size[1], rows)
+        watermark = np.zeros_like(dct_y)
+        watermark = cv2.putText(watermark, watermark_text, (text_x, text_y), font, 0.5, (1, 1, 1), 1, cv2.LINE_AA)
+        dct_y += watermark * 0.01
+        watermark_positions.append((text_x, text_y, text_size[0], text_size[1]))
+    
+    idct_y = cv2.idct(dct_y)
+    
+    ycrcb_image[:, :, 0] = idct_y
+    watermarked_image = cv2.cvtColor(ycrcb_image, cv2.COLOR_YCrCb2BGR)
+    
+    non_visible_watermark_image = watermarked_image.copy()
+    
+    watermark_highlight = watermarked_image.copy()
+    for (text_x, text_y, text_w, text_h) in watermark_positions:
+        cv2.putText(watermark_highlight, watermark_text, (text_x, text_y), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+        cv2.rectangle(watermark_highlight, (text_x, text_y - text_h), (text_x + text_w, text_y), (0, 0, 255), 2)
+    
+    return non_visible_watermark_image, watermark_highlight, watermark_positions
